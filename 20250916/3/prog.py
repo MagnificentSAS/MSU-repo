cf4d23354a48c0deecfde0d5cc3e68f848b262e8
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-09-20 18:20:55 +0300

    Added checked with hworker exc3 and tests for it

---
+++ 20250916/3/prog.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+for i in range(3):
+    a = n + i
+    for j in range(3):
+        b = n + j
+        m = a * b
+        res = m
+        s = 0
+        print(a, '*', b, '=', end = ' ')
+        while m != 0:
+            s += m % 10
+            m //= 10
+            if s < 6:
+                continue
+            if s > 6:
+                # print(s, '>6')
+                print(res, end = ' ' if j < 2 else '\n')
+                break
+        else:
+            # print(s, '<=6')
+            print(res if s != 6 else '=)', end = ' ' if j < 2 else '\n')
+# print()
