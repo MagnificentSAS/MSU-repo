2a7d852a8b6513ef12e1edf1fba9e857e00eea77
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-09-23 11:25:37 +0300

    Added transpose of square  matrix

---
+++ 20250923/0/Transpose.py
@@ -0,0 +1,11 @@
+list = []
+while l := input():
+    l = eval(l)
+    list.append(l)
+
+for i, _ in enumerate(list):
+    for j in range(i, len(list)):
+        list[i][j], list[j][i] = list[j][i], list[i][j]
+
+for i in list:
+    print(*i)
