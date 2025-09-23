523838732cbff97658f2fc8ee6abd761f252211d
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-09-23 11:42:28 +0300

    Added second version of Transpose to check if transposable

---
+++ 20250923/0/Transpose_w2.py
@@ -0,0 +1,13 @@
+list = []
+while l := input():
+    l = eval(l)
+    list.append(l)
+
+if all([len(l) == len(list) for l in list]):
+
+    for i, _ in enumerate(list):
+        for j in range(i, len(list)):
+            list[i][j], list[j][i] = list[j][i], list[i][j]
+
+    for i in list:
+        print(*i)
