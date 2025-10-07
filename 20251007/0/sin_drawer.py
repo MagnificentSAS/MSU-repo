5646e3865716c30df0454ad7d3f46de97d8d065a
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-10-07 11:37:31 +0300

    Added drawing sin drawer hor

--- 20251007/0/sin_drawer.py
+++ 20251007/0/sin_drawer.py
@@ -4,11 +4,17 @@ W, H = eval(input())
 A, B = eval(input())
 
 def scale(a, b, A, B, x):
-    return x * (B - A) / (b - a) + A
+    return (x-a) * (B - A) / (b - a) + A
 
 
-for line in range(0, H):
-    x = scale(0, H - 1, A, B, line)
+screen = [['.'] * W for _ in range(H)]
+
+for line in range(0, W):
+    x = scale(0, W - 1, A, B, line)
     y = sin(x)
-    k = round((y + 1) * (W - 1) / 2)
-    print(" "*k, "*")
+    k = round(scale(-1,1,0,H-1,y))
+    screen[H - k - 1][line] = '*'
+
+print('\n'.join([''.join(screen[i]) for i in range(H)]))
+
+
