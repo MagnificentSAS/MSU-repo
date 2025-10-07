2c2e2b2128ceb14b40bc479d18a8b92e70e1014c
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-10-07 11:22:46 +0300

    Added scaler function

--- 20251007/0/sin_drawer.py
+++ 20251007/0/sin_drawer.py
@@ -3,8 +3,12 @@ from math import *
 W, H = eval(input())
 A, B = eval(input())
 
+def scale(a, b, A, B, x):
+    return x * (B - A) / (b - a) + A
+
+
 for line in range(0, H):
-    x = line * (B - A) / (H - 1) + A
+    x = scale(0, H - 1, A, B, line)
     y = sin(x)
     k = round((y + 1) * (W - 1) / 2)
     print(" "*k, "*")
