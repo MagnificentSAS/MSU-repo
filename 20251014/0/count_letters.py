c4699b20fa40823fd7e12f2e800280fdb984ee37
bronichev.a.r <s02230075@gse.cs.msu.ru>
2025-10-14 11:01:23 +0300

    count_letters is function now

--- 20251014/0/count_letters.py
+++ 20251014/0/count_letters.py
@@ -1,11 +1,13 @@
-str = input()
+def letter_counter(str):
+    a = set()
+    b = set()
+    for l in str:
+        if l in "aeyuio":
+            a.add(l)
+        if l in "qwrtpsdfghjklzxcvbnm":
+            b.add(l)
 
-a = set()
-b = set()
-for l in str:
-    if l in "aeyuio":
-        a.add(l)
-    if l in "qwrtpsdfghjklzxcvbnm":
-        b.add(l)
+    return len(a), len(b)
 
-print(len(a), len(b))
+# str = input()
+# print(*letter_counter(str))
