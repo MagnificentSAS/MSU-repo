b738ae661a341c61ac3a4693675aca0ba01af374
bronichev.a.r <bronichev.a.r@asvk.cs.msu.ru>
2025-11-18 11:17:28 +0300

    Done exc with stupid words

---
+++ 20251118/0/transform.py
@@ -0,0 +1,3 @@
+print('вопрос', 'вопрос'.encode('cp1251').decode('koi8-r'))
+print('бМХЛЮМХЕ', 'бМХЛЮМХЕ'.encode('koi8-r').decode('cp1251'))
+print('ОХРЮМХЕ', 'ОХРЮМХЕ'.encode('koi8-r').decode('cp1251'))
