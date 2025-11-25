1ef2ef1a0f95d14b8edbdb881c741b8b4645fc66
bronichev.a.r <bronichev.a.r@asvk.cs.msu.ru>
2025-11-25 11:39:50 +0300

    Updated matcha

--- 20251125/0/matacha.py
+++ 20251125/0/matacha.py
@@ -1,10 +1,17 @@
+class CONST:
+    A = 100500
+
 match eval(input()):
     case "QWE":
         print("QWE!")
     case "A" | "B" as s:
         print("Letter", s)
+    case CONST.A:
+        print("StoPyatsot")
+    case int(n) if n > 0:
+        print(f"POSITIVE: {int(n)}")
     case int(n):
-        print(f"INT: {int(n)}")
+        print(f"NOT POSITIVE: {int(n)}")
     case (0, *tail):
         print("Zero tuple", tail)
     case (float(n) | int(n), *tail):
