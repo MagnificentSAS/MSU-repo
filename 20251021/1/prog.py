92477effafc73612d7c29a8deb982a261bfc2cc7
bronichev.a.r <bronichev.a.r@asvk.cs.msu.ru>
2025-12-14 12:18:32 +0300

    Added exc1

---
+++ 20251021/1/prog.py
@@ -0,0 +1,11 @@
+import sys
+
+def fib(m, n):
+    a, b = 1, 1
+    for _ in range(m):
+        a, b = b, a + b
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+exec(sys.stdin.read())
